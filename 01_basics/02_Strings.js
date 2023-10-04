9d782d77ad9d345ba0ddb008a8467f4ee8ca7091
crushikesh1998 <127198915+crushikesh1998@users.noreply.github.com>
2023-10-04 12:29:46 +0000

    Strings info

---
+++ 01_basics/02_Strings.js
@@ -0,0 +1,8 @@
+// modern way of string concat
+const name="Rahul";
+const age = 50
+
+// console.log(`My name is ${name} and age is ${age}`);
+console.log(name.length);
+console.log(name.charAt(3));
+console.log((name.indexOf('u')));
