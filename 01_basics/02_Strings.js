6d25f5b1c039c47ca144751b22054debd672a2ec
crushikesh1998 <127198915+crushikesh1998@users.noreply.github.com>
2023-10-04 12:51:59 +0000

    srings info

--- 01_basics/02_Strings.js
+++ 01_basics/02_Strings.js
@@ -1,8 +1,25 @@
 // modern way of string concat
-const name="Rahul";
-const age = 50
+// const name="Rahul";
+// const age = 50
 
 // console.log(`My name is ${name} and age is ${age}`);
-console.log(name.length);
-console.log(name.charAt(3));
-console.log((name.indexOf('u')));
+// console.log(name.length);
+// console.log(name.charAt(3));
+// console.log((name.indexOf('u')));
+
+// console.log(name.substring(0,4));
+
+
+
+//  const gameName= new String('power Ranger')
+// // console.log(gameName);
+// // console.log(gameName.length);
+// // console.log(gameName.slice(0,3))
+// console.log(gameName)
+// console.log(gameName.trim())
+
+
+// const myName= " power "
+// console.log(myName);
+// console.log(myName.trim());
+
