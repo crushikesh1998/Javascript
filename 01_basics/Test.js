23b316dac36a1b49aede6706a507d4a882a80611
crushikesh1998 <127198915+crushikesh1998@users.noreply.github.com>
2023-10-05 10:14:19 +0000

    objects congifg

---
+++ 01_basics/Test.js
@@ -0,0 +1,55 @@
+// modern way of string concat
+// const name="Rahul";
+// const age = 50
+
+// console.log(`My name is ${name} and age is ${age}`);
+// console.log(name.length);
+// console.log(name.charAt(3));
+// console.log((name.indexOf('u')));
+
+// console.log(name.substring(0,4));
+
+
+
+//  const gameName= new String('power Ranger')
+// // console.log(gameName);
+// // console.log(gameName.length);
+// // console.log(gameName.slice(0,3))
+// console.log(gameName)
+// console.log(gameName.trim())
+
+
+// const myName= " power "
+// console.log(myName);
+// console.log(myName.trim())
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// OBJECTS
+
+const myArr=[0,1,2,3,4]
+// console.log(myArr.length);
+// console.log(myArr.push(9));
+// console.log(myArr.unshift(5));
+// console.log(myArr);
+// console.log(myArr.shift());
+// console.log(myArr.includes(23));
+
+// const newArray=['a','b','c','d']
+
+// const sum = myArr.flat(newArray)
+// console.log(sum);
+
+// const myKey=Symbol("key1");
+// const myDetails={
+//     name:"Rushikesh",
+//     email:"chandanshiv12323@gmail.com",
+//     [myKey]:"newKey",
+//     age: 19,
+//     location:"india",
+// }
+
+// console.log(myDetails);
